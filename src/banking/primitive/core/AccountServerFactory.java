7ebda138fe27e806ff580a8e798b7833eb79965b
Andrew Steinmetz <arsteinm@asu.edu>
2017-04-17 02:18:08 -0700

    initial code

---
+++ src/banking/primitive/core/AccountServerFactory.java
@@ -0,0 +1,23 @@
+package banking.primitive.core;
+
+
+public class AccountServerFactory {
+
+	volatile protected static AccountServerFactory singleton = null;
+
+	protected AccountServerFactory() {
+
+	}
+
+	public static AccountServerFactory getMe() {
+		if (singleton == null) {
+			singleton = new AccountServerFactory();
+		}
+
+		return singleton;
+	}
+
+	public AccountServer lookup() {
+		return new ServerSolution();
+	}
+}
